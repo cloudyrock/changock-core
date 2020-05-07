ac45b2702bd1837011a71a2fc299c3a3dcf5363e
dieppa <aperezdieppa@gmail.com>
2020-05-07 07:14:17 +0100

    minor refactor

--- changock-driver/changock-driver-api/src/main/java/io/changock/driver/api/driver/ForbiddenParametersMap.java
+++ changock-driver/changock-driver-api/src/main/java/io/changock/driver/api/driver/ForbiddenParametersMap.java
@@ -1,10 +1,10 @@
 package io.changock.driver.api.driver;
 
 import io.changock.driver.api.common.ForbiddenParameterException;
+import io.changock.utils.OrSupplier;
 
 import java.util.HashMap;
 import java.util.Optional;
-import java.util.function.Supplier;
 
 /**
  * Map structure in which the key is the non-allowed parameter in changeSets and the value is the class which should be replaced by
@@ -12,17 +12,12 @@ import java.util.function.Supplier;
 public class ForbiddenParametersMap extends HashMap<Class, Class> {
 
 
-  public OrSupplierIfNotPresent throwExceptionIfPresent(Class forbiddenClass) throws ForbiddenParameterException {
+  public OrSupplier<Optional<Object>> throwExceptionIfPresent(Class forbiddenClass) throws ForbiddenParameterException {
     Class replacement;
     if((replacement = get(forbiddenClass)) != null) {
       throw new ForbiddenParameterException(forbiddenClass, replacement);
     }
-    return new OrSupplierIfNotPresent();
+    return new OrSupplier<>();
   }
 
-  public static class OrSupplierIfNotPresent {
-    public Optional<Object> or(Supplier<Optional<Object>> supplier) {
-      return supplier.get();
-    }
-  }
 }
