eaecc23eb62c1b153c5e89ffd31ceab6f67e6d68
dieppa <aperezdieppa@gmail.com>
2020-05-09 16:59:03 +0100

    wip

--- changock-runner/spring-runners/changock-spring-v5-runner/src/main/java/io/changock/runner/spring/v5/ChangockSpring5Runner.java
+++ changock-runner/spring-runners/changock-spring-v5-runner/src/main/java/io/changock/runner/spring/v5/ChangockSpring5Runner.java
@@ -1,17 +1,7 @@
 package io.changock.runner.spring.v5;
 
 import io.changock.driver.api.driver.ConnectionDriver;
-import io.changock.migration.api.exception.ChangockException;
 import io.changock.runner.core.builder.DriverBuilderConfigurable;
-import io.changock.runner.core.builder.RunnerBuilderBase;
-import io.changock.runner.spring.util.SpringDependencyManager;
-import io.changock.utils.CollectionUtils;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public final class ChangockSpring5Runner {
 
@@ -20,26 +10,11 @@ public final class ChangockSpring5Runner {
     return new ChangockSpring5RunnerBuilder();
   }
 
-  public static class ChangockSpring5RunnerBuilder extends RunnerBuilderBase<ChangockSpring5RunnerBuilder, ConnectionDriver> {
+  public static class ChangockSpring5RunnerBuilder extends ChangockSpringBuilderBase<ChangockSpring5RunnerBuilder, ConnectionDriver> {
 
-    private static final String DEFAULT_PROFILE = "default";
-    private ApplicationContext springContext;
 
     private ChangockSpring5RunnerBuilder() {
     }
-
-    /**
-     * Set ApplicationContext from Spring
-     *
-     * @param springContext org.springframework.context.ApplicationContext object to inject
-     * @return Changock builder for fluent interface
-     * @see org.springframework.context.annotation.Profile
-     */
-    public ChangockSpring5RunnerBuilder setSpringContext(ApplicationContext springContext) {
-      this.springContext = springContext;
-      return returnInstance();
-    }
-
 
     public ChangockSpringApplicationRunner buildApplicationRunner() {
       return new ChangockSpringApplicationRunner(
@@ -57,46 +32,11 @@ public final class ChangockSpring5Runner {
           enabled);
     }
 
-    private SpringMigrationExecutor buildExecutorWIthEnvironmentDependency() {
-      return new SpringMigrationExecutor(
-          driver,
-          new SpringDependencyManager(this.springContext),
-          lockAcquiredForMinutes,
-          maxTries,
-          maxWaitingForLockMinutes,
-          metadata
-      );
-    }
-
-    private ProfiledChangeLogService buildProfiledChangeLogService() {
-      if (springContext == null) {
-        throw new ChangockException("ApplicationContext from Spring must be injected to Builder");
-      }
-      Environment springEnvironment = springContext.getEnvironment();
-      List<String> activeProfiles = springEnvironment != null && CollectionUtils.isNotNullOrEmpty(springEnvironment.getActiveProfiles())
-          ? Arrays.asList(springEnvironment.getActiveProfiles())
-          : Collections.singletonList(DEFAULT_PROFILE);
-      return new ProfiledChangeLogService(
-          Collections.singletonList(changeLogsScanPackage),
-          startSystemVersion,
-          endSystemVersion,
-          activeProfiles,
-          annotationProcessor
-      );
-    }
-
     @Override
     protected ChangockSpring5RunnerBuilder returnInstance() {
       return this;
     }
 
-    @Override
-    public void runValidation() {
-      super.runValidation();
-      if (springContext == null) {
-        throw new ChangockException("ApplicationContext from Spring must be injected to Builder");
-      }
-    }
   }
 
 }
