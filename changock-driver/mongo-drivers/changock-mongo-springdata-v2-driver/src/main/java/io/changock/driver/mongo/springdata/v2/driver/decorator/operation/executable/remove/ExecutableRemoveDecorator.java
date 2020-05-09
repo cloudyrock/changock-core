90afc199b016b0bbfe7b6823f9ae7149c9ccd185
Dieppa on the rocks <aperezdieppa@gmail.com>
2020-05-09 15:58:30 +0100

    removed mongo drivers (#54)

--- changock-driver/mongo-drivers/changock-mongo-springdata-v2-driver/src/main/java/io/changock/driver/mongo/springdata/v2/driver/decorator/operation/executable/remove/ExecutableRemoveDecorator.java
+++
@@ -1,7 +0,0 @@
-package io.changock.driver.mongo.springdata.v2.driver.decorator.operation.executable.remove;
-
-import io.changock.driver.api.lock.guard.decorator.Invokable;
-import org.springframework.data.mongodb.core.ExecutableRemoveOperation;
-
-public interface ExecutableRemoveDecorator<T> extends Invokable, ExecutableRemoveOperation.ExecutableRemove<T>, RemoveWithCollectionDecorator<T> {
-}
