5ff08e62d639ff06926b2026446c0c8bfbf7dc47
dieppa <aperezdieppa@gmail.com>
2020-05-16 12:08:15 +0100

    github workflow and tests

---
+++ changock-runner/spring-runners/changock-spring-v5-runner/src/test/java/io/cloudyrock/changock/runner/spring/v5/SpringITest.java
@@ -0,0 +1,14 @@
+package io.cloudyrock.changock.runner.spring.v5;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class SpringITest {
+
+
+  @Test
+  public void shouldFail() {
+    fail();
+  }
+}
