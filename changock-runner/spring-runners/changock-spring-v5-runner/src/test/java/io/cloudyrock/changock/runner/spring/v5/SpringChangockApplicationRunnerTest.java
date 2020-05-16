8ce5494d0b6a3c0d93477c1d78c88ccb5656ad79
Dieppa on the rocks <aperezdieppa@gmail.com>
2020-05-16 12:13:19 +0100

    setup GitHub workflow  (#61)

--- changock-runner/spring-runners/changock-spring-v5-runner/src/test/java/io/cloudyrock/changock/runner/spring/v5/SpringChangockApplicationRunnerTest.java
+++ changock-runner/spring-runners/changock-spring-v5-runner/src/test/java/io/cloudyrock/changock/runner/spring/v5/SpringChangockApplicationRunnerTest.java
@@ -39,6 +39,7 @@ import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
