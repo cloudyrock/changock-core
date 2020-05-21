b9ec461390789ca4b07655c91e34950e69356805
dieppa <aperezdieppa@gmail.com>
2020-05-21 20:06:46 +0100

    wip

--- changock-runner/spring-runners/changock-spring-v5-runner/src/main/java/io/changock/runner/spring/v5/ChangockSpring5.java
+++ changock-runner/spring-runners/changock-spring-v5-runner/src/main/java/io/changock/runner/spring/v5/ChangockSpring5.java
@@ -2,6 +2,7 @@ package io.changock.runner.spring.v5;
 
 import io.changock.driver.api.driver.ConnectionDriver;
 import io.changock.runner.core.builder.DriverBuilderConfigurable;
+import io.changock.runner.spring.v5.config.ChangockSpring5Configuration;
 
 public final class ChangockSpring5 {
 
@@ -14,6 +15,17 @@ public final class ChangockSpring5 {
 
 
     private ChangockSpring5RunnerBuilder() {
+    }
+
+    public ChangockSpring5RunnerBuilder setConfig(ChangockSpring5Configuration config) {
+      return this
+          .addChangeLogsScanPackage(config.getChangeLogsScanPackage())
+          .setLockConfig(config.getLockAcquiredForMinutes(), config.getMaxWaitingForLockMinutes(), config.getMaxTries())//optional
+          .setThrowExceptionIfCannotObtainLock(config.isThrowExceptionIfCannotObtainLock())
+          .setEnabled(config.isEnabled())
+          .setStartSystemVersion(config.getStartSystemVersion())
+          .setEndSystemVersion(config.getEndSystemVersion())
+          .withMetadata(config.getMetadata());
     }
 
     public ChangockSpringApplicationRunner buildApplicationRunner() {
---
+++ changock-runner/spring-runners/changock-spring-v5-runner/src/main/java/io/changock/runner/spring/v5/config/SpringRunnerType.java
@@ -0,0 +1,5 @@
+package io.changock.runner.spring.v5.config;
+
+public enum SpringRunnerType {
+    InitializingBean, ApplicationRunner
+}
