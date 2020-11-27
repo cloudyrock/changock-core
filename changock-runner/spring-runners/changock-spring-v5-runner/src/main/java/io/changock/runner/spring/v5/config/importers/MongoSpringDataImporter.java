7dcb251e7d6c7adedc929e771125c14173dfa582
dieppa <aperezdieppa@gmail.com>
2020-11-27 10:17:27 +0000

    allowing changock an mongock configuration

--- changock-runner/spring-runners/changock-spring-v5-runner/src/main/java/io/changock/runner/spring/v5/config/importers/MongoSpringDataImporter.java
+++ changock-runner/spring-runners/changock-spring-v5-runner/src/main/java/io/changock/runner/spring/v5/config/importers/MongoSpringDataImporter.java
@@ -1,22 +1,23 @@
 package io.changock.runner.spring.v5.config.importers;
 
+import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.MapPropertySource;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class MongoSpringDataImporter implements ContextImporter {
   @Override
   public String[] getPaths(Environment environment) {
 
-    boolean mongock = environment.containsProperty("mongock");
-    boolean changock = environment.containsProperty("changock");
 
     try {
-      return loadSpringDataContextV3();
+      return loadSpringDataContextV3(environment);
     } catch (ClassNotFoundException e) {
       try {
-        return loadSpringDataContextV2();
+        return loadSpringDataContextV2(environment);
       } catch (ClassNotFoundException e2) {
         return null;
       }
@@ -31,22 +32,37 @@ public class MongoSpringDataImporter implements ContextImporter {
     );
   }
 
-  private String[] loadSpringDataContextV2() throws ClassNotFoundException {
+  private String[] loadSpringDataContextV2(Environment env) throws ClassNotFoundException {
     String packageName = "com.github.cloudyrock.mongock.driver.mongodb.springdata.v2.";
-    Class.forName(packageName + "SpringDataMongo2Driver");
-    return new String[]{
-        packageName + "MongockSpringDataV3Configuration",
-        packageName + "MongockSpringDataV2Context"
-    };
+    Class.forName(packageName + "SpringDataMongoV2Driver");
+
+    //temporally until deprecated Mongock runner and config is completely removed
+    String configImport = isChangockConfig((AbstractEnvironment) env)
+        ? "SpringDataMongoV2Configuration"
+        : "MongockSpringDataV2Configuration";
+
+    return new String[]{packageName + configImport, packageName + "SpringDataV2Context"};
   }
 
-  private String[] loadSpringDataContextV3() throws ClassNotFoundException {
+  private String[] loadSpringDataContextV3(Environment env) throws ClassNotFoundException {
     String packageName = "com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.";
-    Class.forName(packageName + "SpringDataMongo3Driver");
-    return new String[]{
-//        packageName + "SpringDataMongoV3Configuration",
-        packageName + "MongockSpringDataV3Configuration",
-        packageName + "MongockSpringDataV3Context"
-    };
+    Class.forName(packageName + "SpringDataMongoV3Driver");
+
+    //temporally until deprecated Mongock runner and config is completely removed
+    String configImport = isChangockConfig((AbstractEnvironment) env)
+        ? "SpringDataMongoV3Configuration"
+        : "MongockSpringDataV3Configuration";
+
+    return new String[]{packageName + configImport, packageName + "SpringDataV3Context"};
   }
+
+  private boolean isChangockConfig(AbstractEnvironment env) {
+    return env.getPropertySources()
+        .stream()
+        .filter(propertySource -> propertySource instanceof MapPropertySource)
+        .map(propertySource -> ((MapPropertySource) propertySource).getSource().keySet())
+        .flatMap(Collection::stream)
+        .anyMatch(key -> key.toLowerCase().startsWith("changock."));
+  }
+
 }
