db3abdaa20042c218ac005a4bc0d0ba928fe3f56
dieppa <aperezdieppa@gmail.com>
2020-12-23 10:00:12 +0000

    rebase1

--- changock-driver/changock-driver-api/src/main/java/io/changock/driver/api/driver/ConnectionDriver.java
+++ changock-driver/changock-driver-api/src/main/java/io/changock/driver/api/driver/ConnectionDriver.java
@@ -8,12 +8,43 @@ import io.changock.driver.api.lock.LockManager;
 import java.util.Set;
 
 public interface ConnectionDriver<CHANGE_ENTRY extends ChangeEntry> extends Validable {
-  boolean isInitialized();
   void initialize();
+
   LockManager getLockManager();
+
   LockManager getAndAcquireLockManager();
+
   ChangeEntryService<CHANGE_ENTRY> getChangeEntryService();
+
   Set<ChangeSetDependency> getDependencies();
+
   ForbiddenParametersMap getForbiddenParameters();
+
   Class getLegacyMigrationChangeLogClass(boolean runAlways);
+
+  void setLockAcquiredForMinutes(long lockAcquiredForMinutes);
+
+  void setMaxWaitingForLockMinutes(long maxWaitingForLockMinutes);
+
+  void setMaxTries(int maxTries);
+
+  void setChangeLogRepositoryName(String changeLogRepositoryName);
+
+  void setLockRepositoryName(String lockRepositoryName);
+
+  void setIndexCreation(boolean indexCreation);
+
+  boolean isInitialized();
+
+  long getLockAcquiredForMinutes();
+
+  long getMaxWaitingForLockMinutes();
+
+  int getMaxTries();
+
+  String getChangeLogRepositoryName();
+
+  String getLockRepositoryName();
+
+  boolean isIndexCreation();
 }
--- changock-driver/changock-driver-core/src/main/java/io/changock/driver/core/driver/ConnectionDriverBase.java
+++ changock-driver/changock-driver-core/src/main/java/io/changock/driver/core/driver/ConnectionDriverBase.java
@@ -14,9 +14,15 @@ public abstract class ConnectionDriverBase<CHANGE_ENTRY extends ChangeEntry> imp
 
   private boolean initialized = false;
   private LockManager lockManager = null;
-  private final long lockAcquiredForMinutes;
-  private final long maxWaitingForLockMinutes;
-  private final int maxTries;
+  private long lockAcquiredForMinutes;
+  private long maxWaitingForLockMinutes;
+  private int maxTries;
+  private String changeLogRepositoryName;
+  private String lockRepositoryName;
+  private boolean indexCreation = true;
+
+  public ConnectionDriverBase() {
+  }
 
   public ConnectionDriverBase(long lockAcquiredForMinutes, long maxWaitingForLockMinutes, int maxTries) {
     this.lockAcquiredForMinutes = lockAcquiredForMinutes;
@@ -24,10 +30,6 @@ public abstract class ConnectionDriverBase<CHANGE_ENTRY extends ChangeEntry> imp
     this.maxTries = maxTries;
   }
 
-  @Override
-  public boolean isInitialized() {
-    return initialized;
-  }
 
   @Override
   public final void initialize() {
@@ -60,9 +62,77 @@ public abstract class ConnectionDriverBase<CHANGE_ENTRY extends ChangeEntry> imp
     return lockManager;
   }
 
+  @Override
+  public boolean isInitialized() {
+    return initialized;
+  }
 
+  @Override
+  public long getLockAcquiredForMinutes() {
+    return lockAcquiredForMinutes;
+  }
+
+  @Override
+  public long getMaxWaitingForLockMinutes() {
+    return maxWaitingForLockMinutes;
+  }
+
+  @Override
+  public int getMaxTries() {
+    return maxTries;
+  }
+
+  @Override
+  public String getChangeLogRepositoryName() {
+    return changeLogRepositoryName;
+  }
+
+  @Override
+  public String getLockRepositoryName() {
+    return lockRepositoryName;
+  }
+
+  @Override
+  public boolean isIndexCreation() {
+    return indexCreation;
+  }
+
+  @Override
+  public void setLockAcquiredForMinutes(long lockAcquiredForMinutes) {
+    this.lockAcquiredForMinutes = lockAcquiredForMinutes;
+  }
+
+  @Override
+  public void setMaxWaitingForLockMinutes(long maxWaitingForLockMinutes) {
+    this.maxWaitingForLockMinutes = maxWaitingForLockMinutes;
+  }
+
+  @Override
+  public void setMaxTries(int maxTries) {
+    this.maxTries = maxTries;
+  }
+
+  @Override
+  public void setChangeLogRepositoryName(String changeLogRepositoryName) {
+    this.changeLogRepositoryName = changeLogRepositoryName;
+  }
+
+  @Override
+  public void setLockRepositoryName(String lockRepositoryName) {
+    this.lockRepositoryName = lockRepositoryName;
+  }
+
+  @Override
+  public void setIndexCreation(boolean indexCreation) {
+    this.indexCreation = indexCreation;
+  }
 
   protected abstract LockRepository getLockRepository();
 
   protected abstract void specificInitialization();
+
+  @Override
+  public void runValidation() throws ChangockException {
+
+  }
 }
