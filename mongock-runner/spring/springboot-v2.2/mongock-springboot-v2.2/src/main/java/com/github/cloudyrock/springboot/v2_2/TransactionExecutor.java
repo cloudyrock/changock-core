8535c2fd8095d677e9e8837f9d68ae8dbb263139
dieppa <aperezdieppa@gmail.com>
2021-03-21 15:49:56 +0000

    wip

--- mongock-runner/spring/springboot-v2.2/mongock-springboot-v2.2/src/main/java/com/github/cloudyrock/springboot/v2_2/TransactionExecutor.java
+++ mongock-runner/spring/springboot-v2.2/mongock-springboot-v2.2/src/main/java/com/github/cloudyrock/springboot/v2_2/TransactionExecutor.java
@@ -1,7 +1,5 @@
 package com.github.cloudyrock.springboot.v2_2;
 
-import java.lang.reflect.InvocationTargetException;
-
 public interface TransactionExecutor {
-  void executionInTransaction(Runnable operation) throws IllegalAccessException, InvocationTargetException;
+  void executionInTransaction(Runnable operation);
 }
