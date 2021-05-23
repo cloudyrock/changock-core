f764eedbbebe95a308b03def6e9fef3e13257787
dieppa <aperezdieppa@gmail.com>
2021-05-23 17:24:40 +0100

    refactored

--- mongock-runner/spring/springboot/mongock-springboot/src/main/java/com/github/cloudyrock/springboot/MongockSpringboot.java
+++ mongock-runner/spring/springboot/mongock-springboot/src/main/java/com/github/cloudyrock/springboot/MongockSpringboot.java
@@ -4,26 +4,25 @@ package com.github.cloudyrock.springboot;
 import com.github.cloudyrock.mongock.config.MongockConfiguration;
 import com.github.cloudyrock.mongock.runner.core.executor.ExecutorFactory;
 import com.github.cloudyrock.mongock.runner.core.executor.operation.Operation;
-import com.github.cloudyrock.mongock.runner.core.executor.operation.change.MigrationBuilder;
 import com.github.cloudyrock.mongock.runner.core.executor.operation.change.MigrationOp;
 import com.github.cloudyrock.springboot.base.SpringbootBuilderBase;
 
 public final class MongockSpringboot {
 
   //TODO javadoc
-  public static MigrationBuilderImpl<Boolean> builder() {
-    return new MigrationBuilderImpl<>(new MigrationOp(), new ExecutorFactory<>(), new MongockConfiguration());
+  public static SpringBootBuilder<Boolean> builder() {
+    return new SpringBootBuilder<>(new MigrationOp(), new ExecutorFactory<>(), new MongockConfiguration());
   }
 
 
-  public static class MigrationBuilderImpl<RETURN_TYPE> extends SpringbootBuilderBase<MigrationBuilderImpl<RETURN_TYPE>, RETURN_TYPE, MongockConfiguration>  {
+  public static class SpringBootBuilder<RETURN_TYPE> extends SpringbootBuilderBase<SpringBootBuilder<RETURN_TYPE>, RETURN_TYPE, MongockConfiguration>  {
 
-    private MigrationBuilderImpl(Operation<RETURN_TYPE> op, ExecutorFactory<MongockConfiguration> executorFactory, MongockConfiguration config) {
+    private SpringBootBuilder(Operation<RETURN_TYPE> op, ExecutorFactory<MongockConfiguration> executorFactory, MongockConfiguration config) {
       super(op, executorFactory, config);
     }
 
     @Override
-    public MigrationBuilderImpl<RETURN_TYPE> getInstance() {
+    public SpringBootBuilder<RETURN_TYPE> getInstance() {
       return this;
     }
   }
