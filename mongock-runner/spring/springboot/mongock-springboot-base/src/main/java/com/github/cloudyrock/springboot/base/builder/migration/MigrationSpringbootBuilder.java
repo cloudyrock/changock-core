59bbc41f95989e5e7b5b63abd34537fa1884cea6
dieppa <aperezdieppa@gmail.com>
2021-05-24 12:01:30 +0100

    codacy review

--- mongock-runner/spring/springboot/mongock-springboot-base/src/main/java/com/github/cloudyrock/springboot/base/builder/migration/MigrationSpringbootBuilder.java
+++ mongock-runner/spring/springboot/mongock-springboot-base/src/main/java/com/github/cloudyrock/springboot/base/builder/migration/MigrationSpringbootBuilder.java
@@ -1,11 +1,6 @@
 package com.github.cloudyrock.springboot.base.builder.migration;
 
 import com.github.cloudyrock.mongock.config.MongockConfiguration;
-import com.github.cloudyrock.mongock.runner.core.builder.migration.MigrationBuilderBase;
-import com.github.cloudyrock.springboot.base.MongockApplicationRunner;
-import com.github.cloudyrock.springboot.base.MongockInitializingBeanRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationEventPublisher;
 
 //TODO javadoc
 public interface MigrationSpringbootBuilder extends MigrationSpringbootBuilderBase<MigrationSpringbootBuilder, MongockConfiguration> {
