94c3172924167c9c0a5669c5e257ddfa60f0e139
Dieppa on the rocks <aperezdieppa@gmail.com>
2021-06-25 21:28:36 +0100

    Feature/dynamic proxy (#173)

--- mongock-driver/mongock-driver-api/src/test/java/com/github/cloudyrock/mongock/driver/api/lock/guard/proxy/LockGuardProxyFactoryTest.java
+++ mongock-driver/mongock-driver-api/src/test/java/com/github/cloudyrock/mongock/driver/api/lock/guard/proxy/LockGuardProxyFactoryTest.java
@@ -3,9 +3,11 @@ package com.github.cloudyrock.mongock.driver.api.lock.guard.proxy;
 
 import com.github.cloudyrock.mongock.driver.api.lock.LockManager;
 import com.github.cloudyrock.mongock.driver.api.lock.guard.proxy.util.ContentHandlerFactoryImpl;
+import com.github.cloudyrock.mongock.driver.api.lock.guard.proxy.util.FinalClass;
 import com.github.cloudyrock.mongock.driver.api.lock.guard.proxy.util.InterfaceType;
 import com.github.cloudyrock.mongock.driver.api.lock.guard.proxy.util.InterfaceTypeImpl;
 import com.github.cloudyrock.mongock.driver.api.lock.guard.proxy.util.InterfaceTypeImplNonLockGuarded;
+import com.github.cloudyrock.mongock.driver.api.lock.guard.proxy.util.SomeClass;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -15,7 +17,9 @@ import java.net.ContentHandlerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.github.cloudyrock.mongock.util.test.ReflectionUtils.getImplementationFromLockGuardProxy;
 import static com.github.cloudyrock.mongock.util.test.ReflectionUtils.isProxy;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
@@ -64,11 +68,11 @@ public class LockGuardProxyFactoryTest {
   }
 
   //failing in local but not in CI
-//  @Test
-//  public void shouldReturnProxyWithRightImplementation() {
-//    Object implementation = getImplementationFromLockGuardProxy(getRawProxy(new InterfaceTypeImpl(), InterfaceType.class));
-//    assertEquals(InterfaceTypeImpl.class, implementation.getClass());
-//  }
+  @Test
+  public void shouldReturnProxyWithRightImplementation() {
+    Object implementation = getImplementationFromLockGuardProxy(getRawProxy(new SomeClass(), SomeClass.class));
+    assertEquals(SomeClass.class, implementation.getClass());
+  }
 
   @Test
   public void ShouldReturnNull_ifTargetIsNull() {
@@ -76,8 +80,13 @@ public class LockGuardProxyFactoryTest {
   }
 
   @Test
-  public void ShouldNotReturnProxy_ifTargetNotInterface() {
-    assertFalse(isProxy(getRawProxy(new InterfaceTypeImpl(), InterfaceTypeImpl.class)));
+  public void ShouldReturnProxy_ifTargetNotInterface() {
+    assertTrue(isProxy(getRawProxy(new FinalClass(), InterfaceTypeImpl.class)));
+  }
+
+  @Test
+  public void ShouldNotReturnProxy_ifTargetIsFinal() {
+    assertFalse(isProxy(getRawProxy(new FinalClass(), FinalClass.class)));
   }
 
   @Test
---
+++ mongock-driver/mongock-driver-api/src/test/java/com/github/cloudyrock/mongock/driver/api/lock/guard/proxy/util/FinalClass.java
@@ -0,0 +1,4 @@
+package com.github.cloudyrock.mongock.driver.api.lock.guard.proxy.util;
+
+public final class FinalClass {
+}
