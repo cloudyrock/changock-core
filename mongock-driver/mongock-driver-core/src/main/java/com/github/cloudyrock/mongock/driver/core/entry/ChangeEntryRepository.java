bbf27d86546c967b25d680cee01d2ae7a38ceb11
dieppa <aperezdieppa@gmail.com>
2021-06-28 18:37:19 +0100

    wip

--- mongock-driver/mongock-driver-core/src/main/java/com/github/cloudyrock/mongock/driver/core/entry/ChangeEntryRepository.java
+++ mongock-driver/mongock-driver-core/src/main/java/com/github/cloudyrock/mongock/driver/core/entry/ChangeEntryRepository.java
@@ -2,7 +2,6 @@ package com.github.cloudyrock.mongock.driver.core.entry;
 
 import com.github.cloudyrock.mongock.driver.api.entry.ChangeEntry;
 import com.github.cloudyrock.mongock.driver.api.entry.ChangeEntryService;
-import com.github.cloudyrock.mongock.driver.core.common.EntityRepository;
 
 public interface ChangeEntryRepository<CHANGE_ENTRY extends ChangeEntry> extends ChangeEntryService<CHANGE_ENTRY>{
 
